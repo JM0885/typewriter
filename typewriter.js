ea098167638cb02687d829f0181345b7adafe98f
Jared Mosher <jared.mosher@gmail.com>
2020-12-01 14:19:07 +0000

    File that simulates a typewriter by using setTimeout process in a loop

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+
+let timer = 1000;
+for (const char of sentence) {
+  setTimeout( () => {
+     process.stdout.write(char) }, timer);
+     timer += 100;
+}
+setTimeout(() => {
+  process.stdout.write("\n");
+}, timer);
